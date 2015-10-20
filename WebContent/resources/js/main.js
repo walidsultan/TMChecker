05e931a6e50edcc0a0ce957ea8c1d0a7808f06b3
Walid Sultan <walidalysultan@gmail.com>
2015-10-19 20:31:12 -0500

    Vibration test

--- WebContent/resources/js/main.js
+++ WebContent/resources/js/main.js
@@ -28,4 +28,28 @@ function getState() {
 	$('.state').text(state);
 	$('.heartRate').text(heartRate);
 
+	playBeep();
+	vibrate();
+}
+
+// Show a custom alert
+//
+function showAlert() {
+    navigator.notification.alert(
+        'You are the winner!',  // message
+        'Game Over',            // title
+        'Done'                  // buttonName
+    );
+}
+
+// Beep three times
+//
+function playBeep() {
+    navigator.notification.beep(3);
+}
+
+// Vibrate for 2 seconds
+//
+function vibrate() {
+    navigator.notification.vibrate(2000);
 }
